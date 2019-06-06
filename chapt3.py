4072da4bc4c7aef6a881f1254f68561e1141ecc7
Sow <jenaba.sow@va1macyd0tujhd4.newcorp.com>
2019-06-06 16:45:27 -0400

    Print a message if a var is less than 10, and a diff message if a var is greater than or equal to 10

--- chapt3.py
+++ chapt3.py
@@ -2,3 +2,10 @@
 print("Hi my name is Jenaba!")
 print("I am a rising junior at Howard University.")
 print("I am a computer science major")
+
+#Print a message if a var is less than 10, and a diff message if a var is greater than or equal to 10
+var = 8
+if var < 10:
+    print("The variable is less than 10!")
+else:
+    print("The varible is less than or equal to 10!")
