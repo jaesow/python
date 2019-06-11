91421bcf3916d9758dd05e5d68c468abf56a0db1
Sow <jenaba.sow@va1macyd0tujhd4.newcorp.com>
2019-06-11 16:26:54 -0400

    function that takes a number as an input and returns that number squared

---
+++ chpt4challenges.py
@@ -0,0 +1,10 @@
+# write a function that takes a number as an input and returns that number squared
+
+def squared_func():
+    x = input("typer a number:")
+    x = int(x)
+    x_squared = x **2
+    print(x_squared)
+
+squared_func()
+
