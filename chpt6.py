42621af60eac625279d2dbc0756a8f825f30102d
jenaba.sow <jenaba.sow@asurion.com>
2019-06-24 16:16:55 -0400

    program that collects two strings from a user, inserts them into the string

--- chpt6.py
+++ chpt6.py
@@ -1,8 +1,14 @@
 # chapter 6
 
-#print every character in the string "Camus"
+#1. print every character in the string "Camus"
 
 string = "Camus"
 print( string [0], string [1], string [2], string [3], string [-1] )
 
-#
+#2. Write a program that collects two strings from a user, inserts them into the string "Yesterday I wrote a [response_one]. I sent it to [response_two]!
+
+response_one = input("Enter a word: ")
+response_two = input("Enter another word: ")
+print("Yesturday I wrote a " + response_one + "." + " I sent it to " + response_two + "!")
+
+
